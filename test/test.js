436a7ee348f3737b60f4535682603ecc38ed9d36
Emma Karayiannis <emma.karayiannis@gmail.com>
2019-05-02 15:24:35 +0100

    Failing test for fizz

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
 var assert = require('assert');
 
 var fizzbuzz = require('../src/index.js');
+var jsc = require('jsverify');
 
 describe('fizzbuzz', () => {
   it('will return a string of 1 when given 1 as an argument', () => {
@@ -8,5 +9,12 @@ describe('fizzbuzz', () => {
   });
   it('will return a string of 2 when given 2 as an argument', () => {
     assert.strictEqual(fizzbuzz(2), '2');
+  });
+  it('will return a string  when given any number as an argument', () => {
+    const allChecksPass = jsc.checkForall(jsc.integer, (number) => fizzbuzz(number) === `${number}`);
+    assert.strictEqual(allChecksPass, true);
+  })
+  it('will return a string of fizz when given a multiple of 3 as an argument', () => {
+    assert.strictEqual(fizzbuzz(3), 'fizz');
   })
 });
